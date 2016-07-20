ca06935af746d36f2f79b794db85a894ecef1298
Matt Blair <blair1618@gmail.com>
2016-07-20 18:53:40 -0400

    Add interface to offset the map position within the viewport
    Uses 'principle point offset' based on:
    http://ksimek.github.io/2013/08/13/intrinsic/
    http://ksimek.github.io/2013/06/18/calibrated-cameras-and-gluperspective/

--- core/src/tangram.cpp
+++ core/src/tangram.cpp
@@ -556,6 +556,20 @@ int Map::getCameraType() {
 
 }
 
+void Map::setViewOffset(int xPixels, int yPixels) {
+
+    m_view->setOffset(xPixels, yPixels);
+
+}
+
+void Map::getViewOffset(int *xPixels, int *yPixels) {
+
+    auto offset = m_view->offset();
+    *xPixels = offset.x;
+    *yPixels = offset.y;
+
+}
+
 void Map::addDataSource(std::shared_ptr<DataSource> _source) {
     if (!m_tileManager) { return; }
     std::lock_guard<std::mutex> lock(m_tilesMutex);
